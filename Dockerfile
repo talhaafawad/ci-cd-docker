eaeff5a67c8e926a792c0bca9db3e6d97a38dc32
Talha Bin Fawad <TalhaBin.Fawad@ge.com>
2025-04-04 14:28:22 +0100

    python-app-updated

--- Dockerfile
+++ Dockerfile
@@ -1,18 +1,18 @@
 # Use an official lightweight Python image
 FROM python:3.11-slim
 
-# Set the working directory inside the container
+# Set the working directory
 WORKDIR /app
 
-# Copy the requirements file and install dependencies
+# Copy dependency file and install
 COPY requirements.txt .
 RUN pip install --no-cache-dir -r requirements.txt
 
-# Copy the rest of the application code
+# Copy the rest of the code
 COPY . .
 
-# Expose port 5000 (so Docker can forward requests)
+# Expose port
 EXPOSE 5000
 
-# Command to run the application
+# Run the app using Flask's dev server
 CMD ["python", "app.py"]
--- app.py
+++ app.py
@@ -1,10 +1,21 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://flask_db_l8ys_user:3S02PJf9cSaburqEyYIFj8dTicQ4F85w@dpg-cvnsuk15pdvs73fs853g-a.oregon-postgres.render.com/flask_db_l8ys'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
 
 @app.route("/")
 def home():
     return "Hello, CI/CD with Docker & GitHub Actions!"
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
